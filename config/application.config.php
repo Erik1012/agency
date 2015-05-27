1aaf2dbf792f2647caec65b06c36e0d1a8cc76e5
Erik <erikpolachek@gmail.com>
2015-05-27 14:33:21 +0300

    blog

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,7 @@ return array(
         'Application',
 		  'ZfcBase',
         'ZfcUser',
+		  'blog',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
--- vendor/SxBlog/Module.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace SxBlog;
-
-/**
- * This file is placed here for compatibility with Zendframework 2's ModuleManager.
- * It allows usage of this module even without composer.
- * The original Module.php is in 'src/SxBlog' in order to respect PSR-0
- */
-require_once __DIR__ . '/src/SxBlog/Module.php';
